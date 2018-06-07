45922023ef0f6a2d9f7d6a6cfb3cf0afe51c9b9d
huxx <perfekta213@gmail.com>
2018-06-08 00:14:39 +0900

    1

---
+++ 12.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:/Users/test/Desktop/1.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_naver(object):
+    def setupUi(self, naver):
+        naver.setObjectName("naver")
+        naver.resize(398, 81)
+        self.URL_Box = QtWidgets.QLineEdit(naver)
+        self.URL_Box.setGeometry(QtCore.QRect(20, 40, 271, 21))
+        self.URL_Box.setText("")
+        self.URL_Box.setObjectName("URL_Box")
+        self.label = QtWidgets.QLabel(naver)
+        self.label.setGeometry(QtCore.QRect(20, 10, 56, 21))
+        self.label.setObjectName("label")
+        self.Down_Button = QtWidgets.QPushButton(naver)
+        self.Down_Button.setGeometry(QtCore.QRect(300, 40, 75, 23))
+        self.Down_Button.setObjectName("Down_Button")
+
+        self.retranslateUi(naver)
+        self.URL_Box.returnPressed.connect(self.Down_Button.click)
+        self.Down_Button.clicked.connect(naver.slot1_1st)
+        QtCore.QMetaObject.connectSlotsByName(naver)
+
+    def retranslateUi(self, naver):
+        _translate = QtCore.QCoreApplication.translate
+        naver.setWindowTitle(_translate("naver", "Dialog"))
+        self.label.setText(_translate("naver", "URL"))
+        self.Down_Button.setText(_translate("naver", "Download"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    naver = QtWidgets.QDialog()
+    ui = Ui_naver()
+    ui.setupUi(naver)
+    naver.show()
+    sys.exit(app.exec_())
+
---
+++ d.py
@@ -0,0 +1,85 @@
+# // window widget code
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, QApplication
+from PyQt5.QtCore import QObject, pyqtSlot, QThread
+import sys
+
+# // widget UI setting
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.button_start = QPushButton('Start', self)
+        self.button_cancel = QPushButton('Cancel', self)
+        self.label_status = QLabel('status!!', self)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.button_start)
+        layout.addWidget(self.button_cancel)
+        layout.addWidget(self.label_status)
+
+        self.setFixedSize(400, 200)
+
+    @pyqtSlot(int)
+    def updateStatus(self, status):
+        self.label_status.setText('{}'.format(status))
+
+class Example(QObject):
+
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+
+        self.gui = Window()
+        self.worker = Worker()  # 백그라운드에서 돌아갈 인스턴스 소환
+        self.worker_thread = QThread()  # 따로 돌아갈 thread를 하나 생성
+        self.worker.moveToThread(self.worker_thread)  # worker를 만들어둔 쓰레드에 넣어줍니다
+        self.worker_thread.start()  # 쓰레드를 실행합니다.
+
+        self._connectSignals()  # 시그널을 연결하기 위한 함수를 호출[출처] [Python3] PyQt5 | QThread-pyqtSignal/Slot Example|작성자 우리동네약사
+
+        self.gui.show()
+
+    def _connectSignals(self):
+        # // gui 의 버튼을 클릭시 연결설정
+        self.gui.button_start.clicked.connect(self.worker.startWork)
+        # // worker에서 발생한 signal(sig_numbers)의 연결 설정
+        self.worker.sig_numbers.connect(self.gui.updateStatus)
+        # // cancel 버튼 연결 설정
+        self.gui.button_cancel.clicked.connect(self.forceWorkerReset)
+
+    # // 쓰레드의 loop를 중단하고 다시 처음으로 대기 시키는 func.
+    def forceWorkerReset(self):
+        if self.worker_thread.isRunning():
+            self.worker_thread.terminate()
+            self.worker_thread.wait()
+            self.worker_thread.start()
+
+# // 추가 모듈
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+import time
+
+# // 백그라운드에서 돌아갈 함수 class
+class Worker(QObject):
+    # // 시그널 객체를 하나 생성합니다.
+    sig_numbers = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+
+    @pyqtSlot()
+    def startWork(self):
+        _cnt = 0
+        while _cnt < 10:
+            _cnt += 1
+            self.sig_numbers.emit(_cnt)
+            print(_cnt)
+            time.sleep(1)
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    example = Example(app)
+    sys.exit(app.exec_())
